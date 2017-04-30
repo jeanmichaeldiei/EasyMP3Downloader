fc0d007a21f772cdfaf467a0fcb1d8dcbae85639
Jean-Michael Diei <jeanmichaeldiei@yahoo.com>
2017-04-30 12:02:06 -0400

    Added functionality where icon turns green when available to download.

--- background.js
+++ background.js
@@ -1,5 +1,30 @@
 // background.js
 
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'loading') {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, {"message": "tab-update"});
+    });
+
+  chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "url-message" ) {
+      var url = request.url;
+      var check = "https://www.youtube.com/watch?v=";
+      var pos = url.indexOf(check);
+      if(pos == 0) {
+        chrome.browserAction.setIcon({
+        path: "iconGreen.png"});
+      }
+      else {
+        chrome.browserAction.setIcon({
+        path: "icon.png"});
+    }
+    }}); 
+  }  
+});
+
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
--- content.js
+++ content.js
@@ -1,21 +1,21 @@
 // content.js
+
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      //var firstHref = $("a[href^='http']").eq(0).attr("href");
+      //Checking if URL is valid
       var firstHref = $(location).attr('href'); //Getting current URL
       console.log(firstHref);
 
-      // This line is new!
-      
-      //check if url is https://www.youtube.com/watch?v=
       var check = "https://www.youtube.com/watch?v="
       var pos = firstHref.indexOf(check);
-      //console.log(pos);
       if(pos == 0) {
-        
         chrome.runtime.sendMessage({"message": "download-url", "url": firstHref});
       }
     }
+    else if( request.message === "tab-update") {
+        chrome.runtime.sendMessage({"message": "url-message", "url": document.location.href});
+      }
   }
 );
